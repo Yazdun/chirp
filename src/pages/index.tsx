f1fb8f29e7a2860a8b5d99e8fb66b9ff2cd5ff20
Yazdun <yazduntube@gmail.com>
2023-03-25 22:16:58 +0430

    commit 1:33:39

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,15 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+
+  const { mutate } = api.posts.create.useMutation();
 
   if (!user) return null;
 
@@ -29,7 +33,10 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="type some emojis!"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
