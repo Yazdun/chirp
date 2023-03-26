cb4c9d13269f4df79b23aec3a715f6aaa53e39fd
Yazdun <yazduntube@gmail.com>
2023-03-26 21:49:43 +0430

    commit 1:37:51

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,14 @@ const CreatePostWizard = () => {
   const { user } = useUser();
   const [input, setInput] = useState("");
 
-  const { mutate } = api.posts.create.useMutation();
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -35,6 +42,7 @@ const CreatePostWizard = () => {
         className="grow bg-transparent outline-none"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
       <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
