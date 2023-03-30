45f41553d341f68b0245159c12fa91ccf3b84df2
Yazdun <yazduntube@gmail.com>
2023-03-30 17:45:28 +0430

    update framer animations on wizard

--- src/components/wizard.tsx
+++ src/components/wizard.tsx
@@ -9,10 +9,9 @@ import { AnimatePresence, motion } from 'framer-motion'
 import cn from 'classnames'
 
 const framer_button = {
-  initial: { scale: 0, opacity: 0 },
-  animate: { scale: 1, opacity: 1 },
-  exit: { scale: 0, opacity: 0 },
-  transition: { duration: 0.3 },
+  initial: { scale: 0 },
+  animate: { scale: 1 },
+  exit: { scale: 0 },
 }
 
 export const CreatePostWizard = () => {
