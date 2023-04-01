a4aea4364493eb5ebd008d2813a55c0c1fb7bffa
Yazdun <yazduntube@gmail.com>
2023-04-02 01:50:45 +0430

    update layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { Navigation } from './navigation'
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="pb-[7rem] md:pb-0">
+    <main className="pb-[7rem] md:pb-5">
       <Navigation />
       {props.children}
     </main>
